e18fa5b2ab6288facc532f26b5971400519fd33f
CanadaHonk <19228318+CanadaHonk@users.noreply.github.com>
2022-03-29 13:00:12 +0100

    fix: Use actual message content instead of testing string

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ goosemodHandlers: {
         const channelId = info.channel.id;
         const guildId = info.channel.guild_id;
         // const mentionedRoles = info.message.mentionRoles;
-        const mentionedRoles = [...'<@&844641221964070963>'.matchAll(/<@&([0-9]{17,18})>/g)].map((x) => x[1]);
+        const mentionedRoles = [...info.message.content.matchAll(/<@&([0-9]{17,18})>/g)].map((x) => x[1]);
 
         const guildRoles = getGuild(guildId).roles;
 
